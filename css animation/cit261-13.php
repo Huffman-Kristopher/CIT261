f18d5634b5a77a0f5e890beab6733a55570b9e84
Huffman-Kristopher <nokins_@hotmail.com>
2017-10-17 16:10:57 -0700

    Add files via upload

---
+++ css animation/cit261-13.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+home page
+-->
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <title>Home | retrund.com</title>
+        <meta name="author" content="Kristopher Huffman">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="LDS mission journals,leather journals,leather diaries, leather purses and leather accessories">
+        <link href="/modules/cit261-13css.css" type="text/css" rel="stylesheet" media="screen">
+       
+</head>
+
+<body>
+<div class="container container-one">
+  <div class="orbiting-circle"></div>
+  </div>
+<div class="container container-two">
+  <div class="morphing-circle"></div>
+</div>
+<div class="container container-three">
+  <div class="open-circle-a"></div>
+  <div class="open-circle-b"></div>
+  </div>
+
+</body>
+<footer>   
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
+                
+	<script src="/modules/analytics.js"></script>
+                                
+ </footer>
+</html>  
