ffed853c86ac235b3b4bd7a9cac6b792934b0ee9
Huffman-Kristopher <nokins_@hotmail.com>
2017-12-09 11:48:59 -0800

    Add files via upload

--- generator/basicGen.php
+++ generator/basicGen.php
@@ -3,16 +3,18 @@
 <link href="/modules/bgenstyle.css" type="text/css" rel="stylesheet" media="screen">
 <script>
 var firstNames = ["John", "Frasier", "Jennifer", "April", "Victor", "Steph", "Conner", "Landon", "Laif", "Gabby", "Ivy", "Tony", "Akatika"];
-var secondNames = ["Sanderson", "Johnson", "Smith", "Taylor", "Thorsson", "Huffman", "Gunderson", "Marion", "Charm"];
+var secondNames = ["Sanderson", "Johnson", "Smith", "Taylor", "Thorsson", "Huffman", "Gunderson", "Marion", "Malikihana"];
 var occNames = ["Black Smith", "Horse Trainer", "Shop Keeper", "Bartender", "Mercenary", "Guard", "Leather Worker", "Farier", "Butcher"];
+var adj = ["Thin", "Frail", "Fat", "Willowy", "Strong", "Nimble", "Lithe", "Bulky", "Delicate"];
+var hair = ["Black", "Red", "Blonde", "Sandy", "Brown", "No", "Grey", "White", "Purple"];
 var charNameVisible = 0;
 
 document.onkeypress = function (e) {
     e = e || window.event;
     if (e.keyCode == 13) {
       if (!charNameVisible) {
-        charNameVisible = 1;
-        var charName = getcharName();
+       charNameVisible = 1;
+      var charName = getcharName();
         document.getElementById("window").getElementsByTagName("cursor")[0].innerHTML = "";
         document.getElementById("charName").innerHTML = charName;
         document.getElementById("charName").style.opacity = 1;
@@ -30,21 +32,34 @@ function getcharName() {
 	var firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
 	var secondName = secondNames[Math.floor(Math.random() * secondNames.length)];
 	var occName = occNames[Math.floor(Math.random() * occNames.length)];
-	return "Your Character " + firstName + " " + secondName + " is a " + occName;
+	var adjs = adj[Math.floor(Math.random() * adj.length)];
+	var hairc = hair[Math.floor(Math.random() * hair.length)];
+	return "Your Character " + firstName + " " + secondName + " is a " + adjs + " " + occName + " with " + hairc + " hair.";
 }
 function checkSupport() {
-  var myObj = document.getElementById("div1").onloadstart;
-  // check storage
   if (typeof(Storage) !== "undefined") {
-    myObj.innerHTML = "Everything checks out, local storage is enabled, you are good to go.";
-  } else {
-    myObj.innerHTML = "LocalStorage isn't supported on your browser.  You Need to run Firefox 3.5 or greater, Internet Exporer 8.0 or greater, Chrome 4.0 or greater or Safari 4.0 or greater. Please update your browser and come back.";
+  localStorage.setItem("good", "Welcome to the Random character generator. This is a tool used to create background characters for use in writing. Please enjoy using this and have fun with some of the crazy combinations.");
+    document.getElementById("window2").innerHTML = localStorage.getItem("good");
+}
+ else {
+    document.getElementById("window2").innerHTML = "LocalStorage isn't supported on your browser.  You Need to run Firefox 3.5 or greater, Internet Exporer 8.0 or greater, Chrome 4.0 or greater or Safari 4.0 or greater. Please update your browser and come back.";
   }
 }
+function doc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("window3").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "code.txt", true);
+  xhttp.send();
+}
 </script>
-<body>
+<body onLoad="checkSupport()">
 
-<div id="topbar"><b>Character Creator</b></div>
+<div id="topbar"><b>Random Character Creator</b>
+</div>
 <div id="generator">
 <div id="header">
   </div>
@@ -54,6 +69,18 @@ function checkSupport() {
   </div>
 </div>
 <div id="div1">
+<div id="header2">
 </div>
+<div id="window2">
+</div>
+</div>
+<div id="doc">
+<div id="header3">
+</div>
+<div id="window3"><br>
+<button type="button" id="button" onClick="doc()"> Show Arrays </button>
+</div>
+</div>
+
 </body>
 </html>
